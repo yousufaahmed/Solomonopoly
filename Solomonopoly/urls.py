c8b22359b42245552591feb58b8833e9dcc0a55e
EliotDe <eliotdeacon@yahoo.com>
2025-02-17 14:44:20 +0000

    make seperate app for api functionality

--- Solomonopoly/urls.py
+++ Solomonopoly/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from myapp.views import CreateUserView
+from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
