67b4935f24f39c55cc09a66552e444ecc0751e5f
yousufaahmed <yousufaa@outlook.com>
2025-03-24 05:03:19 +0000

    added API to update player logo

--- api/serializers.py
+++ api/serializers.py
@@ -13,7 +13,7 @@ class PlayerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Player
-        fields = ["player_id", "user","username", "points", "deck", "campus"]
+        fields = ["player_id", "user","username", "points", "deck", "campus", "logo"]
 
 class PlayerIdOnlySerializer(serializers.ModelSerializer):
     class Meta:
@@ -275,3 +275,9 @@ class PlayerAchievementSerializerUpdate(serializers.ModelSerializer):
             raise serializers.ValidationError("The achievement has already been assigned to the player.")
 
         return PlayerAchievement.objects.create(**validated_data)
+
+
+class PlayerUpdateLogoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ['logo']  # Only allow logo updates
